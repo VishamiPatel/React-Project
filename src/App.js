3b1cee1893a899e2ff22778cc39623969a546478
Mitali-Patel <mishal.patel2212@gmail.com>
2022-09-28 15:39:22 +0530

    Counter added in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
+import Counter from "./components/counter";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Counter />
     </div>
   );
 }
