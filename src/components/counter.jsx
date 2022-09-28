e7718c94304f5743aeaf37f3ae2f06eabf86039e
Mitali-Patel <mishal.patel2212@gmail.com>
2022-09-28 17:01:14 +0530

    in App.js Counter component added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,45 +1,19 @@
 import React, { Component, useState, useEffect } from "react";
 
-// const Counter = () => {
-//   const [count, setCount] = useState(0);
-//   useEffect(() => {
-//     document.title = `Count is ${count}`;
-//   }, [count]);
-//   return (
-//     <div>
-//       <button onClick={() => setCount(count + 1)} className="btn btn-primary">
-//         +
-//       </button>
-//       <button onClick={() => setCount(0)} className=" btn p-10">
-//         Reset
-//       </button>
-//       <div>Count :{count}</div>
-//     </div>
-//   );
-// };
-
-class Counter extends Component {
-  state = {
-    count: 0,
-  };
-  handleIncreament = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-  handleReset = () => {
-    this.setState({ count: 0 });
-  };
-  render() {
-    const { count } = this.state;
-    return (
-      <div>
-        <button onClick={this.handleIncreament} className="m-3">
-          +
-        </button>
-        <button onClick={this.handleReset}>Reset</button>
-        <div>Count :{count}</div>
-      </div>
-    );
-  }
-}
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `Count is ${count}`;
+  }, [count]);
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)} className="m-3">
+        +
+      </button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <div>Count :{count}</div>
+    </div>
+  );
+};
 
 export default Counter;
