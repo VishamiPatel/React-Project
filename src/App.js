c5c4be95ea2f8c9d93df0c69f20283fa3662ecfa
Mitali-Patel <mishal.patel2212@gmail.com>
2022-09-29 09:16:30 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from "./logo.svg";
-import Counter from "./components/counter";
+import YoutubeForm from "./components/counter";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <Counter />
+      <YoutubeForm />
     </div>
   );
 }
